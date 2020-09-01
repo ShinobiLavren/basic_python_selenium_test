a43b8a1c66e13dc3bf6b631838ddb913cc1a20cf
Марина-ПК\Марина <laurynovich.andrei@gmail.com>
2020-09-01 22:23:03 +0300

    Adde incorrect data

--- test.py
+++ test.py
@@ -5,4 +5,5 @@ search_field = driver.find_element_by_id('searchInput').send_keys('Test Automati
 search_button = driver.find_element_by_xpath("//form[@id = 'search-form']/fieldset/button")
 search_button.click()
 assert "Test Automation" in driver.title
+assert "Test 1Automation" in driver.title
 driver.quit()
